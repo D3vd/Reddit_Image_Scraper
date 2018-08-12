6f9844053adffb01386cb23a19db79da9be596a1
Dev-C137 <dev.91096@gmail.com>
2018-08-12 09:02:24 +0530

    Initialized setup process

---
+++ Reddit_image_scraper.py
@@ -0,0 +1,23 @@
+import praw
+import configparser
+
+
+class ClientInfo:
+    id = ''
+    secret = ''
+    user_agent = 'Reddit_Image_Scraper'
+
+
+def get_client_info():
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    id = config["ALPHA"]["client_id"]
+    secret = config["ALPHA"]["client_secret"]
+
+    return id, secret
+
+
+if __name__ == '__main__':
+
+    ClientInfo.id, ClientInfo.secret = get_client_info()
+
